b208b7ae3bf0e1c3871165e0ff720ee8c2ea5d02
felixd3v <fmhcde@gmail.com>
2022-10-05 14:07:32 -0500

    Initial Commit

---
+++ Shared/tip_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  tip_calculatorApp.swift
+//  Shared
+//
+//  Created by fm on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct tip_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
