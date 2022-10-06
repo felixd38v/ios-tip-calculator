3c8593b4e1fc0c7a6e1e47cbbbcf7a155449c7d2
felixd3v <fmhcde@gmail.com>
2022-10-05 19:09:24 -0500

    first commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -3,14 +3,39 @@
 //  Shared
 //
 //  Created by fm on 10/05/22.
-//
+//#imageLiteral(resourceName: "Pasted Graphic 25.png")
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var total = "100"
+    @State var tipPercent = 15.0
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Tip Calculator")
+                .foregroundColor(.blue)
+                .padding()
+
+            HStack {
+                Text("$")
+                TextField("Amount", text: $total)
+                
+            }
+            .padding(.all)
+            
+            HStack {
+                Slider(value: $tipPercent, in: 1...30, step: 1.0)
+                Text("\(Int(tipPercent))%")
+            }
             .padding()
+            if let totalNum = Double(total) {//To avoid the app from crashing when a user types a letter instead of a number, i.e., the app will attempt to perform the calculations ONLY if the type casting is successful
+                Text("Tip Amount: $\(totalNum * tipPercent / 100.0, specifier: "%0.2f")")
+            } else {
+                Text("You typed a letter or symbol by mistake. Please type a number")
+            }
+
+        }
+
     }
 }
 
